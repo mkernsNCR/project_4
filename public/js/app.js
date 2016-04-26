1c3d9875cbb4114368305f8348800279754c3ebf
Matthew Lamar Kerns <matthewkerns@ymail.com>
2016-04-26 11:36:07 -0400

    Added Basic angular controllers

--- public/js/app.js
+++ public/js/app.js
@@ -5,20 +5,41 @@
   .module('bucketList', [
     "ui.router"
   ])
-  .config(Router);
+  .config(Router)
+  .controller("listsIndexController", listsIndexCtrl)
+  .controller("listsShowController", listsShowCtrl)
 
   Router.$inject = ["$stateProvider", "$locationProvider", "$urlRouterProvider"];
   function Router($stateProvider, $locationProvider, $urlRouterProvider) {
     $locationProvider.html5Mode(true);
     $stateProvider
-    .state("main", {
+    .state("listsIndex", {
       url: "/",
-      template: "<h2>This is working!</h2>"
+      templateUrl: "/assets/html/lists-index.html",
+      controller: "listsIndexController",
+      controllerAs: "lIndexVM"
     })
-    .state("test", {
-      url: "/test",
-      template: "<h2>This is also working!</h2>"
+    .state("listsShow", {
+      url: "/lists/:title",
+      templateUrl: "/assets/html/lists-show.html",
+      controller: "listsShowController",
+      controllerAs: "lShowVM"
     });
     $urlRouterProvider.otherwise("/");
+  }
+
+  function listsIndexCtrl() {
+    var vm = this;
+    vm.lists = [
+      {"title": "Project X"},
+      {"title": "Trip Around The World!"},
+      {"title": "Wombats in the Wild!"}
+    ];
+  }
+
+  listsShowCtrl.$inject = ["$stateParams"];
+  function listsShowCtrl($stateParams) {
+    var vm = this;
+    vm.list = $stateParams;
   }
 })();
